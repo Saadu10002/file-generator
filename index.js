45fafd56dd6fbe7ea289f78e29aa894f0eb49e22
Babe <115712127+Saadu10002@users.noreply.github.com>
2023-03-12 23:13:29 +0000

    update the creation of generating manager's details

--- index.js
+++ index.js
@@ -12,4 +12,68 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+//empty array to store team member object
+const teamMembers = []
+//empty array to store team member IDS
+const idList = []
 
+
+
+function startApp () {
+    //creating options for the users
+    function createTeam () {
+        inquirer.prompt([{
+            type:`list`,
+            name: `choice`,
+            message: `what would you like to add?`,
+            choices: [`Manager`, `Engineer`, `Intern`, `None`]
+        }]).then(userInput => {
+            if(userInput.choice ===`Manager`) {
+                addManager();
+            }else if(userInput.choice === `Engineer`) {
+                addEngineer();
+            }else if(userInput.choice === `Intern`) {
+                addIntern();
+            }else {
+                buildTeam();
+            }
+        })
+    }
+}
+//creating manager info
+function createManager() {
+
+    inquirer.prompt([
+        {
+            type: `input`,
+            name: `managerName`,
+            message: `name of the team's manager?`,
+        },
+
+        { type: `input`,
+        name: `managerId`,
+        message: `team's manager id`,
+
+        },
+
+        { type: `input`,
+        name: `managerEmail`,
+        message: `team's manager email?`,
+            
+        },
+
+        {
+            type: `input`,
+            name: `managerOfficeNumber`,
+            message: `what is the office's number of the manager?`
+        }
+    ]).then(managerResponse => {
+        const manager = new manager(managerResponse.managerName, managerResponse.managerId, managerResponse.managerEmail, managerResponse.managerOfficeNumber);
+        teamMembers.push(manager);
+        idList.push(managerResponse.managerId);
+    
+    })
+    createManager();
+}
+
+startApp();
